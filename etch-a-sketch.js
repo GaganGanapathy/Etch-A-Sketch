9e21847cce532cbfb549345b9f930a0032bd57d9
GaganGanapathy <109824692+GaganGanapathy@users.noreply.github.com>
2022-12-10 20:48:43 +0530

    added button and reset feature

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,16 +1,27 @@
 const container= document.querySelector(".container");
+const button= document.querySelector("#reset");
 
-const size= Number(prompt("Enter the grid Size! "))
-container.setAttribute('style', `grid-template-columns: repeat(${size}, 1fr)`);
+function setGrid() {
+    const size= Number(prompt("Enter the grid Size! "))
+    container.setAttribute('style', `grid-template-columns: repeat(${size}, 1fr)`);
+    for(let i=1; i<=size*size; i++) {
+        // creating grid
+        const grid= document.createElement("div");
+        grid.classList.add("grid");
+        grid.addEventListener('click', mark);
+        container.appendChild(grid);
+    }
+}
 
-function mark(e) {
+function reset() {
+    container.innerHTML="";
+    setGrid()
+}
+
+button.addEventListener("click", reset);
+
+function mark() {
     this.setAttribute('style', 'background-color: black');
 }
 
-for(let i=1; i<=size*size; i++) {
-    // creating grid
-    const grid= document.createElement("div");
-    grid.classList.add("grid");
-    grid.addEventListener('click', mark);
-    container.appendChild(grid);
-}
+setGrid();
