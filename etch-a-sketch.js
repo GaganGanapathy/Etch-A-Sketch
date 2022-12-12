bdd104a531e24954da6a8b47b862d41772281f2a
GaganGanapathy <109824692+GaganGanapathy@users.noreply.github.com>
2022-12-12 12:54:22 +0530

    added random color and clear property

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,5 +1,18 @@
+let click =false;
+let color="black"
+
 const container= document.querySelector(".container");
+container.addEventListener("click", ()=> {
+    click= !click;
+})
 const button= document.querySelector("#reset");
+const random=document.querySelector(".random");
+random.addEventListener("click", ()=> {
+    color="random"});
+const clear= document.querySelector(".clear");
+clear.addEventListener("click",()=>{
+    color="white";
+} )
 
 function setGrid() {
     const size= Number(prompt("Enter the grid Size! "))
@@ -8,9 +21,16 @@ function setGrid() {
         // creating grid
         const grid= document.createElement("div");
         grid.classList.add("grid");
-        grid.addEventListener('click', mark);
+        grid.addEventListener('mouseover', mark);
         container.appendChild(grid);
     }
+}
+
+function randomColor() {
+    let x = Math.floor(Math.random() * 256);        // range is 0-255
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    return `rgb(${x},${y},${z})`;
 }
 
 function reset() {
@@ -20,8 +40,17 @@ function reset() {
 
 button.addEventListener("click", reset);
 
-function mark() {
-    this.setAttribute('style', 'background-color: black');
+function mark(e) {
+    console.log(this)
+    if(click) {
+        if(color== "random") {
+            this.style.backgroundColor= randomColor();
+        }else if (color==="white"){
+            this.removeAttribute("style");
+        }else{
+            this.style.backgroundColor=color;
+        }
+    }
 }
 
 setGrid();
