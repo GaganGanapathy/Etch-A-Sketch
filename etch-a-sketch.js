ea6baa176c91ef6ecab8fa74cd4fbfceea45b47e
Gagan <ganapathi.gagan@gmail.com>
2023-05-26 21:35:46 +0530

    styling and color feature

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,21 +1,47 @@
 let click =false;
 let color="black"
 
+const button= document.querySelector("#reset");
+const random=document.querySelector(".random");
+
+const input = document.querySelector("#num");
+const chosenColor = document.querySelector("#color");
+const form = document.querySelector("form")
+
+const clear= document.querySelector(".clear");
 const container= document.querySelector(".container");
+
+
+
+form.addEventListener("submit", (i) => {
+    const newGrid = Number(i.target[0].value);
+    if (newGrid) {
+        container.innerHTML = ""
+        setGrid(newGrid)
+    } 
+    const newColor = (i.target[1].value);
+    console.log(newColor)
+    color = newColor
+
+    i.preventDefault();
+    input.value=""
+    chosenColor.value=""    
+    // return [i.target[0].value, i.target[1].value]
+})
+
 container.addEventListener("click", ()=> {
     click= !click;
 })
-const button= document.querySelector("#reset");
-const random=document.querySelector(".random");
 random.addEventListener("click", ()=> {
     color="random"});
-const clear= document.querySelector(".clear");
+
+
 clear.addEventListener("click",()=>{
     color="white";
 } )
 
-function setGrid() {
-    const size= Number(prompt("Enter the grid Size! "))
+function setGrid(grids=25) {
+    const size= grids
     container.setAttribute('style', `grid-template-columns: repeat(${size}, 1fr)`);
     for(let i=1; i<=size*size; i++) {
         // creating grid
@@ -40,8 +66,7 @@ function reset() {
 
 button.addEventListener("click", reset);
 
-function mark(e) {
-    console.log(this)
+function mark() {
     if(click) {
         if(color== "random") {
             this.style.backgroundColor= randomColor();
